0bc532cfc2a4e18a72ad8589a29522437df8c6db
seamus-parker <86379563+seamus-parker@users.noreply.github.com>
2024-05-04 16:59:28 -0700

    Add files via upload

---
+++ Board.java
@@ -0,0 +1,19 @@
+
+public class Board {
+
+    private Player[] players;
+
+    private Set[] sets;
+
+    // Set up the board, initialize player locations, money/credits, rank, etc.
+    public void beginGame(int numPlayers){}
+
+    // Move players back to trailers, deal new scenes & replace shot counters
+    public void endDay(){}
+
+    // Determine the winner of the game and return their name
+    public String calculateWinner(String[] names, int[] ranks){ return ""; }
+
+    public Board(){}
+
+}
---
+++ Set.java
@@ -0,0 +1,16 @@
+import java.util.Dictionary;
+
+public class Set {
+    
+    public Dictionary<String, String>[] roles;
+
+    public Set[] neighbors;
+
+    public Scene card;
+
+    // calculate and distribute bonuses to players working on the card
+    public void calculateBonuses(){}
+
+    public Set(){}
+
+}
