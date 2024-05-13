8938b9e65041c3715852ac640327353883bf34c3
seamus-parker <86379563+seamus-parker@users.noreply.github.com>
2024-05-12 22:32:04 -0700

    Update Set.java

--- Set.java
+++ Set.java
@@ -1,16 +1,31 @@
-import java.util.Dictionary;
-
-public class Set {
+public class Set extends Room{
     
-    public Dictionary<String, String>[] roles;
-
-    public Set[] neighbors;
+    public Role[] roles;
 
     public Scene card;
+
+    public int shots;
+
+    public int shotCounters;
+
+
+    public void removeShotCounter() {
+        this.shotCounters--;
+    }
+
+    public void replaceShotCounters() {
+        this.shotCounters = this.shots;
+    }
 
     // calculate and distribute bonuses to players working on the card
     public void calculateBonuses(){}
 
-    public Set(){}
+    public Set(String myName, Room[] myNeighbors, Role[] myRoles, int numShots) {
+        super.setName(myName);
+        super.setNeighbors(myNeighbors);
+        this.roles = myRoles;
+        this.shots = numShots;
+        this.replaceShotCounters();
+    }
 
 }
