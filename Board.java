cf8418f8fc1098aa37c46a00ee819da13d13bd97
seamus-parker <86379563+seamus-parker@users.noreply.github.com>
2024-05-12 22:33:59 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -3,10 +3,10 @@ public class Board {
 
     private Player[] players;
 
-    private Set[] sets;
+    private Room[] sets;
 
     // Set up the board, initialize player locations, money/credits, rank, etc.
-    public void beginGame(int numPlayers){}
+    public void beginGame(int numPlayers) {}
 
     // Move players back to trailers, deal new scenes & replace shot counters
     public void endDay(){}
