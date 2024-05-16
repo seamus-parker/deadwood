731ab87bd703df126242dcc1bfd98c6e0d51e4fe
TrPonce <treverponce@yahoo.com>
2024-05-16 16:47:46 -0700

    Update Set.java
    implement Bonuses function

--- Set.java
+++ Set.java
@@ -18,7 +18,42 @@ public class Set extends Room{
     }
 
     // calculate and distribute bonuses to players working on the card
-    public void calculateBonuses(){}
+     public void calculateBonuses(int budget){
+        int[] bonusResults = new int[6];
+        for (int i = 0; i < 6; i++){
+            if (i<budget){
+                int result = dieRoll();
+                bonusResults[i] = result;
+            }else{
+                bonusResults[i]=0;
+            }
+        }        
+        Player[] playerArray = card.getSortedPlayers();
+        int playersOnCard = playerArray.length;
+        Arrays.sort(bonusResults);
+        if (playersOnCard = 3){
+            playerArray[2].addDollars(bonusResults[5]);
+            playerArray[2].addDollars(bonusResults[2]);
+            playerArray[1].addDollars(bonusResults[4]);
+            playerArray[1].addDollars(bonusResults[1]);
+            playerArray[0].addDollars(bonusResults[3]);
+            playerArray[0].addDollars(bonusResults[0]);
+        }else if (playersOnCard = 2){
+            playerArray[1].addDollars(bonusResults[5]);
+            playerArray[1].addDollars(bonusResults[2]);
+            playerArray[1].addDollars(bonusResults[4]);
+            playerArray[0].addDollars(bonusResults[1]);
+            playerArray[0].addDollars(bonusResults[3]);
+            playerArray[0].addDollars(bonusResults[5]);
+        }else if (playersOnCard = 1){
+            playerArray[0].addDollars(bonusResults[0]);
+            playerArray[0].addDollars(bonusResults[1]);
+            playerArray[0].addDollars(bonusResults[2]);
+            playerArray[0].addDollars(bonusResults[3]);
+            playerArray[0].addDollars(bonusResults[4]);
+            playerArray[0].addDollars(bonusResults[5]);
+        }
+    }
 
     public Set(String myName, String[] myNeighbors, Role[] myRoles,
                int numShots, int myX, int myY, int myH, int myW) {
