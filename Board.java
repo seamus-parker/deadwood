68e691e630140d15bf594acba14ec256d8c795f0
seamus-parker <86379563+seamus-parker@users.noreply.github.com>
2024-05-16 17:34:27 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -12,6 +12,8 @@ public class Board {
 
     private int days;
 
+    private Player activePlayer;
+
     void shuffle() {
         Random rnd = new Random();
         for (int i = this.deck.length - 1; i > 0; i--) {
@@ -19,6 +21,32 @@ public class Board {
             Scene a = this.deck[index];
             this.deck[index] = this.deck[i];
             this.deck[i] = a;
+        }
+    }
+
+    public Room getRoomByName(String name) {
+        for (int i = 0; i < sets.length; i++) {
+            if (sets[i].getName().equals(name)) {
+                return sets[i];
+            }
+        }
+        return new Room();
+    }
+
+    public Player getActivePlayer() {
+        return this.activePlayer;
+    }
+
+    public void setActivePlayer(int num) {
+        this.activePlayer = this.players[num];
+    }
+
+    public void nextPlayer() {
+        if (this.activePlayer.getId() == this.players.length-1) {
+            this.setActivePlayer(0);
+        }
+        else {
+            this.setActivePlayer(this.activePlayer.getId()+1);
         }
     }
 
@@ -33,30 +61,39 @@ public class Board {
 
     public Board(String[] playerNames, int numPlayers, Room[] sets,
                  Scene[] deck, int[][] upgrades) {
+
+        this.sets = sets;
+        this.deck = deck;
+        this.upgrades = upgrades;
         Player[] myPlayers = new Player[numPlayers];
         this.days = 4;
         int credits = 0;
         int rank = 1;
+
         if (numPlayers < 4) {
             this.days = 3;
         }
+
         if (numPlayers == 5) {
             credits = 2;
         }
+
         if (numPlayers == 6) {
             credits = 4;
         }
+
         if (numPlayers > 6) {
             rank = 2;
         }
+
         for (int i = 0; i < numPlayers; i++) {
-            // construct players
-            //myPlayers[i] = new Player()
+            myPlayers[i] = new Player(playerNames[i], i, this.getRoomByName("trailers"));
+            myPlayers[i].addCredits(credits);
+            myPlayers[i].upgradeRank(rank);
         }
+
         this.players = myPlayers;
-        this.sets = sets;
-        this.deck = deck;
-        this.upgrades = upgrades;
+
         shuffle();
     }
 
